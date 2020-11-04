e1c37fdfa641e9cf0e97e40c1f7389824030a577
eyitene solomon <solomoneyitene@gmail.com>
2020-11-04 08:38:56 +0100

    initial setup for frontend

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Da menu</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Damenu</h1>
+</body>
+</html>
