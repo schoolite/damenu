7d680128821d472e35e383d3dc87370d0eb6836d
eyitene solomon <solomoneyitene@gmail.com>
2020-11-05 11:37:29 +0100

    added meal forms with js

--- addmenu.php
+++ addmenu.php
@@ -37,36 +37,36 @@
                     <div class="container">
                         <ul class="days-list">
                             <li class="day">
-                                <a href="# " class="active item">Monday</a>
+                                <a href="# " class="active item" data-day="1">Monday</a>
                             </li>
                         
                             <li class="day">
-                                <a href="#" class="item">Tuesday</a>
+                                <a href="#" class="item" data-day="2">Tuesday</a>
                             </li>
                             <li class="day">
-                                <a href="#" class="item">Wednesday</a>
+                                <a href="#" class="item" data-day="3">Wednesday</a>
                             </li>
                             <li class="day">
-                                <a href="#" class="item">Thursday</a>
+                                <a href="#" class="item" data-day="4">Thursday</a>
                             </li>
                             <li class="day">
-                                <a href="#" class="item">Friday</a>
+                                <a href="#" class="item" data-day="5">Friday</a>
                             </li>
                             <li class="day">
-                                <a href="#" class="item">Saturday</a>
+                                <a href="#" class="item" data-day="6">Saturday</a>
                             </li>
                             <li class="day">
-                                <a href="#" class="item">Sunday</a>
+                                <a href="#" class="item" data-day="7">Sunday</a>
                             </li>
                         </ul>
 
                         <div class="meal">
-                            <div class="meal-day" data-meal-day="1">
+                            <div class="meal-day show" data-meal-day="1">
                                 <!-- meal-day = 1 is for monday -->
                                 <form action="#" method="post">
 
+                                    <input type="hidden" name="day" value="1">
                                     <div class="form-group-inline">
-
                                         <div class="form-group">
                                             <select name="category" id="category" class="form-control">
                                                 <option value="1">Breakfast</option>
@@ -80,7 +80,171 @@
                                         <div class="form-group">
                                             <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for monday">
                                         </div>  
+
                                     </div>
+                                    <a href="#" class="btn-link add-meal" data-for="1">Add meal </a>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day " data-meal-day="2">
+                                <!-- meal-day = 2 is for tuesday -->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="2">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for tuesday">
+                                        </div>  
+
+                                    </div>
+                                    <a href="#" class="btn-link add-meal" data-for="2">Add meal </a>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day" data-meal-day="3">
+                                <!-- meal-day = 3 is for wednesday -->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="3">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for wednesday">
+                                        </div>  
+
+                                    </div>
+                                        <a href="#" class="btn-link add-meal" data-for="3">Add meal </a>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day" data-meal-day="4">
+                                <!-- meal-day = 4 is for thursday -->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="4">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for thursday">
+                                        </div>  
+
+                                    </div>
+                                        <a href="#" class="btn-link add-meal" data-for="4">Add meal </a>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day" data-meal-day="5">
+                                <!-- meal-day = 5 is for friday-->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="5">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for friday">
+                                        </div>  
+
+                                        <a href="#" class="btn-link add-meal" data-for="5">Add meal </a>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day" data-meal-day="6">
+                                <!-- meal-day = 6 is for saturday -->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="6">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for saturday">
+                                        </div>  
+
+                                    </div>
+                                    <a href="#" class="btn-link add-meal" data-for="6">Add meal </a>
+
+                                    <button type="submit" class="btn btn-primary">Save</button>
+                                </form>
+                            </div>
+                            <div class="meal-day" data-meal-day="7">
+                                <!-- meal-day = 7 is for sundary -->
+                                <form action="#" method="post">
+
+                                    <input type="hidden" name="day" value="1">
+                                    <div class="form-group-inline">
+                                        <input type="hidden" name="day" value="7">
+                                        <div class="form-group">
+                                            <select name="category" id="category" class="form-control">
+                                                <option value="1">Breakfast</option>
+                                                <option value="2">Launch</option>
+                                                <option value="3">Dinner</option>
+                                                <option value="4">Braunch</option>
+                                                <option value="5">Desert</option>
+                                            </select>
+                                        </div>
+    
+                                        <div class="form-group">
+                                            <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for sunday">
+                                        </div>  
+
+                                    </div>
+                                    <a href="#" class="btn-link add-meal" data-for="7">Add meal </a>
 
                                     <button type="submit" class="btn btn-primary">Save</button>
                                 </form>
--- js/main.js
+++ js/main.js
@@ -1,31 +1,154 @@
 window.addEventListener('load', function(){
-
-
-    /**
-     * Getting all days of the week list to toggle active states
-     */
-
-    const days = document.querySelectorAll('.day .item');
     
+    // querySelectorAll shortener
+    const selectAll = (prop)=>{
+        return  document.querySelectorAll(prop);
+    }
+
+    // querySelector for one shortener
+    const selectOne = (prop)=>{
+        return document.querySelector(prop);
+    }
+
+    /** Getting all days of the week list to toggle active states*/
+    
+    const days = selectAll('.day .item');
+
+
+
+    /* Getting all meal days element to show each meal based on which is clicked for */
+    const mealDays = selectAll('.meal .meal-day');
+
+
+
+    // Getting add meal buttons
+    const addMealBtn = selectAll('.add-meal');
+
+
+
+    // current Day selected in the form
+    let currentDayForm  = 1;
+
+
+    // function to show /hide each meal day
+    const showCurrentMealDay = (day) => {
+       
+        mealDays.forEach(meal => {
+            if(meal.dataset.mealDay !== day){
+                meal.classList.add('hide');
+                meal.classList.remove('show');
+            }else{
+                meal.classList.add('show');
+                meal.classList.remove('hide');
+            }
+        });
+    }
+
+   
+
+    // handling clicking of each day menu on the page
     days.forEach(day => {
         day.addEventListener('click', function() {
             /**
-             * On click all elements with class of actives get the class removed
-             * current element should get the active class
+             * On click if elements contains  class of active get the class removed
+             * current element then get the active class 
              */
             removeDaysActiveClass();
             this.classList.add('active');
 
-        })
-    })
+            // assigning the day value to the currentDayForm to keep track of it
+            currentDayForm = this.dataset.day;
 
+            showCurrentMealDay(currentDayForm);
+            
+
+
+        });
+    });
+
+
+
+    // function to handle removing of active class
     const removeDaysActiveClass = () => {
         days.forEach(day => {
             day.classList.contains('active') ? day.classList.remove('active') : '';
         })
     }
+
+    const buildForm = function(el, parentTracker){
+        el.innerHTML = `
+            
+            <input type="hidden" name="day" value="${parentTracker}">
+            <div class="form-group">
+                <select name="category" id="category" class="form-control">
+                    <option value="1">Breakfast</option>
+                    <option value="2">Launch</option>
+                    <option value="3">Dinner</option>
+                    <option value="4">Braunch</option>
+                    <option value="5">Desert</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <input type="text" name="food_name" id="food_name" class="form-control" placeholder="what is for sunday">
+            </div> 
     
     
     
+        `;
+    }
+
+    const formCountChecker = function(parentTracker){
+        let forms = selectAll('.form-group-inline');
+        let count = 0;
+        forms.forEach(function(form){
+            if(form.parentNode.parentNode.dataset.mealDay == parentTracker){
+               console.log(form);
+            }
+           
+        })
+        // console.log(count);
+
+        return (count == 5) ? false : true;
+    };
+
+    let nodeLevel = 3;
+
+    addMealBtn.forEach(btn => {
+
+        btn.addEventListener('click', (e)=> {
+            e.preventDefault();
+
+            // get parent node identity
+            let parentTracker = btn.parentNode.parentNode.dataset.mealDay;
+
+            // get mealElement based on the parentTracker
+            let mealELement =  [...mealDays].filter(meal => meal.dataset.mealDay == parentTracker)[0];
+           
+            // element where the new form will be place after
+
+            let targetFormElement = mealELement.childNodes[3].childNodes[nodeLevel];
+            nodeLevel++;
+            // console.log(mealELement.childNodes[3].childNodes);
+            let el = document.createElement('div');
+            el.classList.add('form-group-inline');
+
+            buildForm(el, parentTracker);
+            
+            formCountChecker(parentTracker);
+
+            targetFormElement.after(el)
+            
+            
+        });
+
+    });
+
+  
+
+
+    
+    
+
    
-})
+});
